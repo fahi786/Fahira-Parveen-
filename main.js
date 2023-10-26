752f932431b330730e97026aec3a87be81940cf9
fahi786 <149080409+fahi786@users.noreply.github.com>
2023-10-26 19:56:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Full Stack Developer", "College Student", "Programmer"],
+    typeSpeed:20,
+    backSpeed:20,
+    backdDelay:1000,
+    loop:true
+});
